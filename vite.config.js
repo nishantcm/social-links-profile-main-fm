ad83f01aacc70b0d7837220f8ca6441ddbe241fb
nishantcm <cmnishantwork@gmail.com>
2024-03-25 21:59:03 +0530

    Update vite config file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ const path = require('path')
 
 export default {
   root: path.resolve(__dirname, 'src'),
+  build: {
+    outDir: path.join(__dirname, "dist"),
+     },
   resolve:{
     alias:{
         '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
